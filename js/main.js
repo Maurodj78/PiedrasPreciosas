59ee6b317c833c2c162adaa3344851a2c867189f
Mauro Alonzi <102665234+Maurodj78@users.noreply.github.com>
2023-10-24 11:25:05 -0300

    validaciones agregadas al formulario de contacto

--- js/main.js
+++ js/main.js
@@ -8,4 +8,33 @@ window.onscroll = () => {
 	}
 }
 
+function validarFormulario() {
+	var nombre = document.getElementById("nombre").value;
+	var email = document.getElementById("email").value;
+	var mensaje = document.getElementById("mensaje").value;
 
+	if (nombre === "" || email === "" || mensaje === "") {
+		alert("Por favor, completa todos los campos.");
+		return false;
+	}
+
+	var expresion = /^[a-zA-Z]+$/;  
+
+    if (!expresion.test(nombre)) {
+        alert("El campo 'nombre' no debe contener caracteres numéricos.");
+        return false;
+    }
+
+	var expresionEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (!expresionEmail.test(email)) {
+		alert("El correo electrónico no tiene un formato válido.");
+		return false;
+	}
+
+	if (mensaje.trim() === "") {
+		alert("El campo 'mensaje' no puede ser solo espacios en blanco.");
+		return false;
+	}
+
+	return true;
+}
--- pages/js/main.js
+++ pages/js/main.js
@@ -8,46 +8,34 @@ window.onscroll = () => {
 	}
 }
 
-// Obtén una referencia al formulario y al botón de conversión
-const conversionForm = document.getElementById('conversionForm');
-const convertButton = document.getElementById('convertButton');
-const amountInput = document.getElementById('amountInput');
+function validarFormulario() {
+	var nombre = document.getElementById("nombre").value;
+	var email = document.getElementById("email").value;
+	var mensaje = document.getElementById("mensaje").value;
 
-// Función de conversión
-function convertToUSD(amountInARS) {
-  const apiKey = 'ecb5cd4e452e4a82f7a01458eb3f79e9'; // Reemplaza 'TU_CLAVE_API' con tu clave de API
+	if (nombre === "" || email === "" || mensaje === "") {
+		alert("Por favor, completa todos los campos.");
+		return false;
+	}
 
-  // Construye la URL de la solicitud
-  const apiUrl = `https://data.fixer.io/api/convert?access_key=${apiKey}&from=ARS&to=USD&amount=${amountInARS}`;
+	var expresion = /^[a-zA-Z]+$/;  
 
-  // Realiza la solicitud GET
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      if (data.success) {
-        const convertedAmount = data.result; // El resultado de la conversión
-        console.log(`${amountInARS} ARS equivale a ${convertedAmount} USD`);
-        // Puedes mostrar el resultado en tu página web aquí
-      } else {
-        console.error('Error en la solicitud a la API de Fixer');
-      }
-    })
-    .catch(error => {
-      console.error('Error de red:', error);
-    });
+    if (!expresion.test(nombre)) {
+        alert("El campo 'nombre' no debe contener caracteres numéricos.");
+        return false;
+    }
+
+	var expresionEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (!expresionEmail.test(email)) {
+		alert("El correo electrónico no tiene un formato válido.");
+		return false;
+	}
+
+	if (mensaje.trim() === "") {
+		alert("El campo 'mensaje' no puede ser solo espacios en blanco.");
+		return false;
+	}
+	
+	return true;
 }
-
-// Agrega un evento de envío del formulario
-conversionForm.addEventListener('submit', function (event) {
-  event.preventDefault(); // Previene la acción predeterminada del formulario (recargar la página)
-  
-  const amountInARS = parseFloat(amountInput.value); // Obtén la cantidad ingresada por el usuario
-
-  if (!isNaN(amountInARS)) {
-    convertToUSD(amountInARS); // Llama a la función de conversión con la cantidad ingresada
-  } else {
-    console.error('Ingrese una cantidad válida en pesos argentinos.');
-  }
-});
-
 
